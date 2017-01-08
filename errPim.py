93ffffc535a37385f5e9f32e9bac3a2e02e05d1b
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2017-01-08 18:43:56 +0100

    Adding configuration parameters

--- errPim.py
+++ errPim.py
@@ -1,16 +1,19 @@
 # This is a skeleton for Err plugins, use this to get started quickly.
 
 from errbot import BotPlugin, botcmd
-import ConfigParser
+import configparser
 import subprocess
 import os
 import time
-import urllib2
+#import urllib2
+import urllib.request
+import requests
 import re
 import sys
 import json
 from bs4 import BeautifulSoup
-from cStringIO import StringIO
+#from cStringIO import StringIO
+import io #import StringIO
 from twitter import *
 import facebook
 
@@ -18,10 +21,31 @@ import facebook
 class ErrPim(BotPlugin):
     """An Err plugin skeleton"""
     min_err_version = '1.6.0' # Optional, but recommended
-    max_err_version = '4.0.4' # Optional, but recommended
+    max_err_version = '4.3.4' # Optional, but recommended
+
+    def get_configuration_template(self):
+        """ configuration entries """
+        config = {
+            'pathMail': '',
+        }
+        return config
+
+    def _check_config(self, option):
+
+        # if no config, return nothing
+        if self.config is None:
+            return None
+        else:
+            # now, let's validate the key
+            if option in self.config:
+                return self.config[option]
+            else:
+                return None
+
+
 
     def ptw(self, msg, args):
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read([os.path.expanduser('~/.rssTwitter')])
 
         CONSUMER_KEY = config.get("appKeys", "CONSUMER_KEY")
@@ -35,16 +59,16 @@ class ErrPim(BotPlugin):
                                    CONSUMER_SECRET)
         t = Twitter(auth=authentication)
         reply = t.statuses.update(status = args)
-        return reply["created_at"]
+        return "OK" #reply["created_at"]
 
     def pfb(self, msg, args):
-         config = ConfigParser.ConfigParser()
+         config = configparser.ConfigParser()
          config.read([os.path.expanduser('~/.rssFacebook')])
 
          oauth_access_token= config.get("Facebook", "oauth_access_token")
 
          graph = facebook.GraphAPI(oauth_access_token)
-         return graph.put_object("me", "feed", message = args)
+         return "Ok" # graph.put_object("me", "feed", message = args)
 
     def search(self, msg, args):
          arg='/usr/bin/sudo /usr/bin/mairix %s'%args
@@ -57,26 +81,23 @@ class ErrPim(BotPlugin):
          yield "Indexando ..." 
          yield self.search(msg, args)
 
-# sudo ls -l /var/vmail/ra-amon.lan/f.tricas/Maildir/.Search/cur 
-#lrwxrwxrwx 1 root root 120 May 11 17:44 123456789.119299.mairix:2,S -> /var/vmail/ra-amon.lan/f.tricas/Maildir/Maildir/.Research.weblogs/cur/1427325746.M403724P15101.ra-amon,S=9615,W=9766:2,S
-#lrwxrwxrwx 1 root root 122 May 11 17:44 123456789.119300.mairix:2,S -> /var/vmail/ra-amon.lan/f.tricas/Maildir/Maildir/.Research.weblogs/cur/1437524577.M889587P11219.ra-amon,S=13980,W=14185:2,S
-
     @botcmd
     def sf(self, msg, args):
          yield self.search(msg, args)
 
-         path = '/var/vmail/ra-amon.lan/f.tricas/Maildir'
+         path = self._check_config('pathMail')
          arg='/usr/bin/sudo /bin/ls -l %s/.Search/cur' % path
          p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
          data = p.communicate()
          
          folders = []
          
-         for i in data[0].split('\n')[1:]:
-         	j = i[i.find('/.')+2:]
-         	folder = j[:j.find('/')]
-         	if folder and 'mairix' not in folder and not folder in folders: 
-         		folders.append(folder)
+         
+         for i in data[0].decode("utf-8").split('\n')[1:]:
+             j = i[i.find('/.')+2:]
+             folder = j[:j.find('/')]
+             if folder and 'mairix' not in folder and not folder in folders: 
+                 folders.append(folder)
 
          yield folders
 
@@ -91,33 +112,33 @@ class ErrPim(BotPlugin):
          else:
             parada = "CAMPUS RIO EBRO"
          
-         request = urllib2.Request(url)
-         request.add_header("Accept",  "application/json")
-         
-         resProc = json.load(StringIO(urllib2.urlopen(request).read()))
+         request = urllib.request.Request(url)
+         headers = {"Accept":  "application/json"}
+         response = requests.get(url, headers = headers)
+         resProc = response.json() 
          tit = 0
          if resProc["totalCount"] > 0:
             for i in range(int(resProc["totalCount"])):
                 if (resProc["result"][i]["title"].find(parada) >= 0):
                    if (tit == 0):
-			   yield "Parada: " + resProc["result"][i]["title"] + " (" + parada + ")"
-			   tit = 1
-                   timeDest = {}
-                   if "destinos" in resProc["result"][i]:
-                       for j in range(len(resProc["result"][i]["destinos"])):
-                          if (resProc["result"][i]["destinos"][j]["destino"]) in timeDest:
-                             timeDest[resProc["result"][i]["destinos"][j]["destino"]].append(resProc["result"][i]["destinos"][j]["minutos"])
-                          else:
-                             timeDest[resProc["result"][i]["destinos"][j]["destino"]] = []
-                             timeDest[resProc["result"][i]["destinos"][j]["destino"]].append(resProc["result"][i]["destinos"][j]["minutos"])
-                       for j in timeDest.keys():
-                          time1 = timeDest[j][0]
-                          if (len(timeDest[j]) > 1):
-		                  time2 = timeDest[j][1]
-		                  cad = cadTemplate % (time1, time2, j)
-                          else:
-		                  cad = cadTemplate % (time1, time1, j)
-                          yield cad
+                       yield "Parada: " + resProc["result"][i]["title"] + " (" + parada + ")"
+                       tit = 1
+                       timeDest = {}
+                       if "destinos" in resProc["result"][i]:
+                           for j in range(len(resProc["result"][i]["destinos"])):
+                              if (resProc["result"][i]["destinos"][j]["destino"]) in timeDest:
+                                 timeDest[resProc["result"][i]["destinos"][j]["destino"]].append(resProc["result"][i]["destinos"][j]["minutos"])
+                              else:
+                                 timeDest[resProc["result"][i]["destinos"][j]["destino"]] = []
+                                 timeDest[resProc["result"][i]["destinos"][j]["destino"]].append(resProc["result"][i]["destinos"][j]["minutos"])
+                           for j in timeDest.keys():
+                               time1 = timeDest[j][0]
+                               if (len(timeDest[j]) > 1):
+                                    time2 = timeDest[j][1]
+                                    cad = cadTemplate % (time1, time2, j)
+                               else:
+                                    cad = cadTemplate % (time1, time1, j)
+                           yield cad
                    else:
                       yield "No hay trenes"
          else:
@@ -126,10 +147,10 @@ class ErrPim(BotPlugin):
 
     @botcmd
     def dir(self, msg, args):
-         url='http://diis.unizar.es/?q=directorio'
+         url='http://diis/?q=directorio'
          
-         req = urllib2.Request(url) 
-         directorio = urllib2.urlopen(req)
+         req = urllib.request.Request(url) 
+         directorio = urllib.request.urlopen(req)
          
          soup = BeautifulSoup(directorio)
          
@@ -151,7 +172,7 @@ class ErrPim(BotPlugin):
          yield self.ptw(msg, args)
 
     @botcmd
-    def fb(self, msg, args):	
+    def fb(self, msg, args):    
          yield self.pfb(msg, args)
 
     @botcmd
